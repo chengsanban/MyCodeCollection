e11bd6e440ea1fb69e9304a9af4219e14aba97b0
zhou.chen <chengsanban@outlook.com>
2020-03-24 22:45:19 +0800

    delete file

--- JXHomework/Front-end/server/model/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/school', { useNewUrlParser: true ,useUnifiedTopology: true})
-    .then( doc => {
-        console.log('db connet success!!!');
-    }).catch(err => {
-        console.log('db connet fail!!!');
-    });
-
-    
